e36f991cce6f5dbecc8053539589627db2ab1b68
bozidarsk <bozidarkabahcijski@gmail.com>
2023-08-04 08:54:35 +0300

    Added default config and style.

--- ProjectConfig.cs
+++ ProjectConfig.cs
@@ -21,8 +21,8 @@ public static partial class Config
 	public static string Css { private set; get; }
 	public static string ConfigDir { private set; get; }
 
-	private static readonly string DefaultConfig = "WindowMarginTop=0\nWindowMarginRight=0\nWindowMarginBottom=0\nWindowMarginLeft=0\nWindowAnchor[]=Top,Right";
-	private static readonly string DefaultCss = "";
+	private static readonly string DefaultConfig = "WindowWidth=300\nWindowHeight=50\nWindowMarginTop=10\nWindowMarginRight=10\nWindowMarginBottom=0\nWindowMarginLeft=0\nWindowAnchor[]=Top,Right\nVolumeMute=\nVolumeUnmute=\n\n# format must be 'SYSTEMNAME\\x1dFRIENDLYNAME\\n...'\nGetSinksCommand=pactl --format=json list sinks | sed -E 's/index/\\n/g' | grep name | sed -E 's/\\\":[0-9]+,\\\"state\\\":\\\"[^\\\"]+\\\",\\\"name\\\":\\\"([^\\\"]+)\\\",\\\"description\\\":\\\"([^\\\"]+)\\\".+/\\1\\x1d\\2/'\n\nSetDefaultSinkCommand=pactl set-default-sink '%value%'\nGetDefaultSinkCommand=pactl get-default-sink | tr -d '\\n'\n\nSetVolumeCommand=pactl set-sink-volume @DEFAULT_SINK@ '%value%'%\nGetVolumeCommand=pactl get-sink-volume @DEFAULT_SINK@ | tr -d '\\n' | sed -E 's/.+\\s+([0-9]+)%.+/\\1/'\n\nSetMuteCommand=pactl set-sink-mute @DEFAULT_SINK@ '%value%'\nGetMuteCommand=pactl get-sink-mute @DEFAULT_SINK@ | sed -E 's/Mute: yes/true/' | sed -E 's/Mute: no/false/'";
+	private static readonly string DefaultCss = "* {\n    font-family: \"Source Code Pro Bold\";\n    font-weight: bold;\n    transition: 0.2s;\n}\n\n#window {\n    border: 2px solid @theme_selected_bg_color;\n    border-radius: 8px;\n    background-color: @theme_base_color;\n}\n\n#box {\n    padding: 10px;\n}\n\n#dropdown {\n}\n\n#dropdown cellview {\n    margin-top: 2px;\n}\n\n#box-controlls {\n    margin-top: 5px;\n}\n\n#mute {\n    margin-right: 5px;\n}\n\n#mute label {\n    font-family: \"Font Awesome 6 Sharp\";\n    font-size: 15px;\n}\n\n#slider value {\n    margin-left: 5px;\n    color: @theme_text_color;\n}";
 	private static readonly string DefaultConfigDir = Environment.GetEnvironmentVariable("HOME") + "/.config/sound-menu";
 
 	private static readonly Option[] OptionsDefinition = 
