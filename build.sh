a8ccdf97ce1e7ad190b95c8870ff81f2d63eb7a9
bozidarsk <bozidarkabahcijski@gmail.com>
2023-08-19 15:37:32 +0300

    Improved build script.

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # runtime: mono, gtk-sharp-3, gtk-layer-shell
 
 mcs '-recurse:*.cs' -pkg:gtk-sharp-3.0 -out:sound-menu
-chmod 755 ./sound-menu
-sudo chown root:root ./sound-menu
-sudo mv ./sound-menu /usr/local/bin
+if [[ $? -eq 0 ]]; then
+	chmod 755 ./sound-menu
+	sudo chown root:root ./sound-menu
+	sudo mv ./sound-menu /usr/local/bin
+else
+	echo "Build failed."
+	exit 1
+fi
